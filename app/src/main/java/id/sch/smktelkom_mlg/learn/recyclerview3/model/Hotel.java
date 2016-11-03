9f0f3cb1d40d56d48efed5edb3f787897fedb715
Rindi Tiara PA <rinditiarapa@gmail.com>
2016-11-03 08:24:24 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Win_8.1 on 10/31/2016.
+ */
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
